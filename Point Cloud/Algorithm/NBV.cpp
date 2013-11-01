ba607e0fd83ae32d9b58fb518bebee6a2ba46b93
sunway <sunwayliving@gmail.com>
2013-11-01 12:26:59 +0800

    *add some basic functions to NBV

--- Point Cloud/Algorithm/NBV.cpp
+++ Point Cloud/Algorithm/NBV.cpp
@@ -11,3 +11,44 @@ NBV::~NBV()
 {
 
 }
+
+void
+NBV::run()
+{
+  if (para->getBool("Run Build Grid"))
+  {
+    runBuildGrid();
+    return;
+  }
+}
+
+void
+NBV::setInput(DataMgr *pData)
+{
+  if (!pData->isOriginalEmpty())
+  {
+    CMesh *_original = pData->getCurrentOriginal();
+    if (NULL == _original)
+    {
+      cout<<"ERROR: NBV original == NULL !"<<endl;
+      return;
+    }
+
+    original = _original;
+  }else
+  {
+    cout<<"ERROR: NBV::setInput empty!"<<endl;
+  }
+}
+
+void
+NBV::clear()
+{
+  original = NULL;
+}
+
+void
+NBV::runBuildGrid()
+{
+
+}
--- Point Cloud/Algorithm/NBV.h
+++ Point Cloud/Algorithm/NBV.h
@@ -12,6 +12,14 @@ public:
   NBV(RichParameterSet* _para);
   ~NBV();
 
+  void run();
+  void setInput(DataMgr *pData);
+  void setParameterSet(RichParameterSet *_para) { para = _para;}
+  RichParameterSet * getParameterSet() { return para;}
+  void clear();
+
+private:
+  void runBuildGrid();
 
 private:
   RichParameterSet *para;
